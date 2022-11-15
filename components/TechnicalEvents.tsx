7c9b1904fd905210f0fdd8158efcb868cbb172e7
Manikandan Mohan <mmohan@fivedtech.com>
2022-11-15 17:32:31 +0530

    Added some event tiles

--- components/TechnicalEvents.tsx
+++ components/TechnicalEvents.tsx
@@ -1,9 +1,77 @@
 import styles from "../styles/TechnicalEvents.module.css";
+import { Card, Image, Text, Badge, Button, Group, Grid } from "@mantine/core";
+import { UserInfo } from "./UserInfo";
 
 const TechnicalEvents = () => {
   return (
     <div>
-      This is TechnicalEvents Component
+      <h2>Technical Events</h2>
+      <Grid justify={"space-around"}>
+            <Grid.Col style={{maxWidth: 400}} md={4} >
+                <Card shadow="sm" p="lg" radius="md" withBorder>
+                  <Text size={"xl"} weight={500}>Event #1</Text>
+                    <ul>
+                      <li>
+                        <Text size="md" color="dimmed">This is rule # 1</Text>
+                      </li>
+                      <li>
+                        <Text size="md" color="dimmed">This is rule # 2</Text>
+                      </li>
+                      <li>
+                        <Text size="md" color="dimmed">This is rule # 3</Text>
+                      </li>
+                      <li>
+                        <Text size="md" color="dimmed">This is rule # 4</Text>
+                      </li>
+                    </ul>
+
+                    <hr />
+                    <UserInfo name="Co-Ordinator" avatar="" email="someone@gmail.com" phone="927348273947" title="" />
+                </Card>
+            </Grid.Col>
+            <Grid.Col style={{maxWidth: 400}}  md={4}>
+              <Card shadow="sm" p="lg" radius="md" withBorder>
+                  <Text weight={500}>Event #2</Text>
+                    <ul>
+                      <li>
+                        <Text size="md" color="dimmed">This is rule # 1</Text>
+                      </li>
+                      <li>
+                        <Text size="md" color="dimmed">This is rule # 2</Text>
+                      </li>
+                      <li>
+                        <Text size="md" color="dimmed">This is rule # 3</Text>
+                      </li>
+                      <li>
+                        <Text size="md" color="dimmed">This is rule # 4</Text>
+                      </li>
+                    </ul>
+                    <hr />
+                    <UserInfo name="Person" avatar="" email="someone@gmail.com" phone="927348273947" title="" />
+              </Card>
+            </Grid.Col>
+            <Grid.Col style={{maxWidth: 400}}  md={4}>
+            <Card shadow="sm" p="lg" radius="md" withBorder>
+                  <Text weight={500}>Event #2</Text>
+                    <ul>
+                      <li>
+                        <Text size="md" color="dimmed">This is rule # 1</Text>
+                      </li>
+                      <li>
+                        <Text size="md" color="dimmed">This is rule # 2</Text>
+                      </li>
+                      <li>
+                        <Text size="md" color="dimmed">This is rule # 3</Text>
+                      </li>
+                      <li>
+                        <Text size="md" color="dimmed">This is rule # 4</Text>
+                      </li>
+                    </ul>
+                    <hr />
+                    <UserInfo name="Co-Ordinator" avatar="" email="someone@gmail.com" phone="927348273947" title="" />
+              </Card>
+            </Grid.Col>
+        </Grid>
     </div>
   );
 };
---
+++ components/UserInfo.tsx
@@ -0,0 +1,53 @@
+import { createStyles, Avatar, Text, Group } from '@mantine/core';
+import { PhoneCall, At } from 'tabler-icons-react';
+
+const useStyles = createStyles((theme) => ({
+  icon: {
+    color: theme.colorScheme === 'dark' ? theme.colors.dark[3] : theme.colors.gray[5],
+  },
+
+  name: {
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+  },
+}));
+
+interface UserInfoIconsProps {
+  avatar: string;
+  name: string;
+  title: string;
+  phone: string;
+  email: string;
+}
+
+export function UserInfo({ avatar, name, title, phone, email }: UserInfoIconsProps) {
+  const { classes } = useStyles();
+  return (
+    <div>
+      <Group noWrap>
+        <div>
+          <Text size="md" sx={{ textTransform: 'uppercase' }} weight={700} color="dimmed">
+            {title}
+          </Text>
+
+          <Text size="sm" weight={500} className={classes.name}>
+            {name}
+          </Text>
+
+          <Group noWrap spacing={10} mt={3}>
+            <At size={16} className={classes.icon} />
+            <Text size="sm" color="dimmed">
+              {email}
+            </Text>
+          </Group>
+
+          <Group noWrap spacing={10} mt={5}>
+            <PhoneCall size={16} className={classes.icon} />
+            <Text size="sm" color="dimmed">
+              {phone}
+            </Text>
+          </Group>
+        </div>
+      </Group>
+    </div>
+  );
+}
