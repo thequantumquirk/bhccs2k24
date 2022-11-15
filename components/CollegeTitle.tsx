5630a8653628f73d69a909b722a17e25194437af
Manikandan Mohan <mmohan@fivedtech.com>
2022-11-15 22:17:33 +0530

    Updated College Title Component

--- components/CollegeTitle.tsx
+++ components/CollegeTitle.tsx
@@ -1,10 +1,29 @@
+import { Container, Text, Grid } from "@mantine/core";
+import Image from "next/image";
 import styles from "../styles/CollegeTitle.module.css";
 
 const CollegeTitle = () => {
   return (
-    <div>
-      This is college Title Component
-    </div>
+    <Container>
+      <div style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center"
+      }}>
+        <Image
+          src={"/bhclogo.png"} alt="BHC college logo"
+          width={80}
+          height={100}/>
+        <div style={{
+          marginLeft: "20px"
+        }}>
+          <h1 style={{
+            marginBottom: "5px"
+          }}>Bishop Heber College (Autonomous)</h1>
+          <Text>College of Excellence</Text>
+        </div>
+      </div>
+    </Container>
   );
 };
 
