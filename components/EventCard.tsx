13e33f8f0e09c3c1b547927949e2c0c5bc45243b
Manikandan Mohan <mmohan@fivedtech.com>
2022-11-15 21:19:33 +0530

    Added Some Styling to Event Cards

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -1,10 +1,24 @@
-import { Card, Text } from "@mantine/core";
+import { Card, Text, createStyles } from "@mantine/core";
+
+const useStyles = createStyles((theme) => ({
+    eventCard: {
+        background: theme.fn.linearGradient(45, '#3457D5', '#00BFFF'),
+    },
+    eventTitle: {
+        color: "white"
+    },
+    eventDescription: {
+        color: "white"
+    }
+}));
 
 const EventCard = ({ event } : { event : any }) => {
+  const { classes, cx } = useStyles();
+
   return (
-    <Card shadow="sm" p="lg" radius="md" withBorder style={{minHeight: "100px"}}>
-        <Text size={"xl"} weight={500}>{ event.title }</Text>
-        <Text size="md" color="dimmed">{ event.description }</Text>
+    <Card className={classes.eventCard} shadow="sm" p="lg" radius="md" withBorder style={{minHeight: "100px"}}>
+        <Text className={classes.eventTitle} size={"xl"} weight={500}>{ event.title }</Text>
+        <Text className={classes.eventDescription} size="md" color="dimmed">{ event.description }</Text>
     </Card>
   );
 };
