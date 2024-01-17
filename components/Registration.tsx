7b58a4d5e53827e6538b442894252a5e7f843fc4
fosslover <fosslover@duck.com>
2024-01-17 17:35:20 +0530

    fix: update event date

--- components/Registration.tsx
+++ components/Registration.tsx
@@ -19,7 +19,7 @@ const Registration = () => {
           <p>The Symposium will be held on</p>
           <div className={styles.eventDateContainer}>
             <CalendarEvent color={"#03a9f4"} className={styles.calendarIcon} />
-            <p>12 Feb, 2024</p>
+            <p>13 Feb, 2024</p>
           </div>
         </div>
       </div>
